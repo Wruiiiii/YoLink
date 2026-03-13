ce31a51ecf05797fb1dce711d2f720677a422a11
Wruiiiii <ruiwang025@gmail.com>
2026-03-12 21:17:59 -0400

    Initial Commit

---
+++ YoLink/YoLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoLinkApp.swift
+//  YoLink
+//
+//  Created by Rae Wang on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct YoLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
